4299c0882d31259441bee18e2bb43fc677e28dc9
Sebastian Biorci <sebipochi@gmail.com>
2025-05-31 09:10:48 -0300

    Elimino contactoPage de navbar

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -14,13 +14,6 @@ function Navbar() {
         Eventos
         <span className='absolute bottom-0 left-0 w-0 h-[2px] bg-MiExpo_white transition-all duration-300 group-hover:w-full'></span>
       </h3>
-      <h3
-        onClick={() => router.push('/contacto')}
-        className='hover:cursor-pointer text-MiExpo_white text-[1rem] sm:text-[1.2rem] lg:text-[1.5rem] font-sans leading-[32px] relative group'
-      >
-        Contacto
-        <span className='absolute bottom-0 left-0 w-0 h-[2px] bg-MiExpo_white transition-all duration-300 group-hover:w-full'></span>
-      </h3>
     </div>
   );
 }
