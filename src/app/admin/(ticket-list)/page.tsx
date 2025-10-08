6963d3c71c25263bbbc92799e0c5deefd31c0f9d
nicofishman <nicourifishman@gmail.com>
2025-10-08 10:59:25 -0300

    fix: timezone

--- src/app/admin/(ticket-list)/page.tsx
+++ src/app/admin/(ticket-list)/page.tsx
@@ -1,7 +1,7 @@
 import { TicketsTable } from '@/components/[N]TicketList/[N]TicketsTable';
 import { columns } from '@/components/[N]TicketList/columns';
 import { trpc } from '@/server/trpc/server';
-import { format } from 'date-fns';
+import { formatInTimeZone } from 'date-fns-tz';
 import { CalendarIcon, ClockIcon, MapPin } from 'lucide-react';
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
@@ -28,12 +28,25 @@ export default async function TicketListPage() {
       <div className='flex flex-wrap items-center justify-center gap-x-3 pb-3'>
         <h3 className='flex items-center gap-x-1 p-2 text-center align-middle text-sm sm:text-base'>
           <CalendarIcon className='inline h-5 w-5' />
-          {format(event!.startingDate, 'dd/MM/yyyy')}
+          {formatInTimeZone(
+            event!.startingDate,
+            'America/Argentina/Buenos_Aires',
+            'dd/MM/yyyy',
+          )}
         </h3>
         <h3 className='flex items-center gap-x-1 p-2 text-center align-middle text-sm sm:text-base'>
           <ClockIcon className='inline h-5 w-5' />
-          {format(event!.startingDate, 'HH:mm')} -{' '}
-          {format(event!.endingDate, 'HH:mm')}
+          {formatInTimeZone(
+            event!.startingDate,
+            'America/Argentina/Buenos_Aires',
+            'HH:mm',
+          )}{' '}
+          -{' '}
+          {formatInTimeZone(
+            event!.endingDate,
+            'America/Argentina/Buenos_Aires',
+            'HH:mm',
+          )}
         </h3>
         <h3 className='flex items-center p-2 text-center text-sm sm:text-base'>
           <MapPin className='inline h-5 w-5' />
